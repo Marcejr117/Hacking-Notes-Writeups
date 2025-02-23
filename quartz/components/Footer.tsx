d07aef8ab5675d3812b90cb8be22866a6e26a8ff
Marcejr117 <ggmarce559@gmail.com>
2025-02-23 20:28:20 +0100

    vault backup: 2025-02-23 20:28:20

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Created by <a href="https://github.com/Marcejr117" target="_blank">Marcejr117</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
