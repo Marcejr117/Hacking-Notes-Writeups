8d8bf71fefab6e925db706a3fa78cd8df9f26ae1
Marcejr117 <ggmarce559@gmail.com>
2025-09-09 03:13:48 +0200

    vault backup: 2025-09-09 03:13:48

--- setup-submodules.sh
+++ setup-submodules.sh
@@ -4,6 +4,9 @@ echo "⚙️  Configurando acceso a submódulos privados..."
 
 # Configura Git para usar tu token
 git config --global url."https://${GITHUB_REPO_CLONE_TOKEN}@github.com/".insteadOf "https://github.com/"
+git config --global url."https://${GITHUB_REPO_CLONE_TOKEN}@github.com/".insteadof "ssh://git@github.com/"
 
 # Inicializa y actualiza los submódulos
+git submodule sync --recursive
 git submodule update --init --recursive
+
